f29e3aa613d12f8a652576499af4e1ec0bfc195e
marurevi <maryrev@hotmail.com>
2022-11-28 02:51:28 -0300

    Add salesTaxes and Total functionality

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 class Product {
-  constructor(name, price, quantity, impoted = false, category = 'other', taxes = 0) {
+  constructor(name, price, quantity, impoted = false, category = 'other') {
     this.name = name;
     this.price = price;
     this.quantity = quantity;
     this.impoted = impoted;
     this.category = category;
-    this.taxes = this.getTaxes();
+    this.taxes = this.getTaxes() || 0;
   }
+
   getTaxes() {
     let taxes = 0;
     if (this.impoted) taxes += 0.05;
@@ -16,10 +17,13 @@ class Product {
 }
 
 const products = [
-  new Product('book', 12.49, 1, false, 'book'),
+  new Product('book', 12.49, 2, false, 'book'),
   new Product('music CD', 14.99, 1, false, 'other'),
   new Product('chocolate bar', 0.85, 1, false, 'food'),
 ];
+
+let result = 0; let
+  salesTaxes = 0;
 
 products.forEach((product) => {
   const productsList = document.getElementById('products-list');
@@ -27,6 +31,8 @@ products.forEach((product) => {
   const price = product.price * product.quantity;
   const taxes = Math.round((price * product.taxes) * 100) / 100;
   const total = (price + taxes).toFixed(2);
+  salesTaxes += parseFloat(taxes);
+  result += parseFloat(total);
 
   trow.innerHTML = `
     <td>${product.name}</td>
@@ -38,3 +44,13 @@ products.forEach((product) => {
   `;
   productsList.appendChild(trow);
 });
+
+const total = document.getElementById('result');
+total.innerHTML = `
+  <td>Total</td>
+  <td></td>
+  <td></td>
+  <td></td>
+  <td>${salesTaxes}</td>
+  <td>${result}</td>
+`;
